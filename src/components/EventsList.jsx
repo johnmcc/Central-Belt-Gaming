6d161893396dc292ab8b46ff3ad8f13299a02abe
John McCollum <johnvmccollum@gmail.com>
2017-09-08 22:08:35 +0100

    Remove date headers

--- src/components/EventsList.jsx
+++ src/components/EventsList.jsx
@@ -3,44 +3,19 @@ import Event from './Event';
 import './EventsList.css';
 var _ = require("lodash");
 
-var days = require("days");
-var months = require("months");
-
 const EventsList = ({events}) => {
     if(!events.length){
         return <img id="loading" src="/images/loading.gif" alt="Loading..." />;
     }
 
     events = _.sortBy(events, ["start_time"]);
-
-    events = _.groupBy(events, (event) => {
-        const day = days[event.start_time.getDay()];
-        const date = event.start_time.getDate();
-        const month = months[event.start_time.getMonth()];
-        const year = event.start_time.getFullYear();
-
-        return `${day} ${date} ${month}, ${year}`;
-    });
-
-    let rows = [];
-
-    for(let day in events){
-        const event_date = new Date(Date.parse(day));
-        rows.push(
-            <div className="eventDay" key={day}>
-                <h3>{day}</h3>
-                <div className="events">
-                    {events[day].map(event => {
-                        return <Event details={event} key={event.id} />
-                    })}
-                </div>
-            </div>
-        );
-    }
-
     return (
         <div id="eventsList">
-            {rows}
+            {events.map(event => {
+                return <Event
+                            details={event}
+                            key={event.id} />
+            })}
         </div>
     );
 };
