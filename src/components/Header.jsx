f11e7e90d9efc61b9247579bdd02d805736db203
John McCollum <johnvmccollum@gmail.com>
2017-09-12 10:37:02 +0100

    Change strapline

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const header = () => {
     return (
         <header>
             <h1>Central Belt Gaming</h1>
-            <h2>Board Gaming events in Glasgow, Edinburgh, and across the central belt of Scotland</h2>
+            <h2>Tabletop Gaming events in Glasgow, Edinburgh, and across the central belt of Scotland</h2>
         </header>
     );
 };
