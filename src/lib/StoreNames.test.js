e90fbebb1def5576f60483efd45f4ab2ff0ce7a6
John McCollum <johnvmccollum@gmail.com>
2017-09-16 20:20:13 +0100

    Tidy up  StoreNames tests

--- src/lib/StoreNames.test.js
+++ src/lib/StoreNames.test.js
@@ -1,14 +1,16 @@
 import StoreNames from './StoreNames';
 
-it("is an object", () => {
-    expect(typeof(StoreNames)).toBe("object");
-});
+describe("StoreNames", () => {
+    it("can be imported", () => {
+        expect(typeof(StoreNames)).toBe("object");
+    });
 
-it("has a length of more than zero", () => {
-    expect(StoreNames.length).not.toBe(0);
-});
+    it("has a length of more than zero", () => {
+        expect(StoreNames.length).not.toBe(0);
+    });
 
-it("should contain certain shops", () => {
-    const expected = ["WarhammerGlasgow"];
-    expect(StoreNames).toEqual(expect.arrayContaining(expected));
+    it("should contain certain shops", () => {
+        const expected = ["WarhammerGlasgow"];
+        expect(StoreNames).toEqual(expect.arrayContaining(expected));
+    });
 });
