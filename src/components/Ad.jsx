04a7621b74f0ffd72f99e35fe1a28a0a4011efe8
John McCollum <johnvmccollum@gmail.com>
2017-09-16 20:02:34 +0100

    Remove inline ad styling

--- src/components/Ad.jsx
+++ src/components/Ad.jsx
@@ -7,7 +7,7 @@ class Ad extends Component {
 
     render () {
         return (
-            <div className="supplemental" style={{ float: "right", width: "50%", height: "100px" }}>
+            <div className="supplemental">
                 <ins className="adsbygoogle"
                      style={{display: "inline-block", width: "100%", height: "100%"}}
                      data-ad-client="ca-pub-2942965289729484"
